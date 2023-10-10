10977b56e893403aace7bcbe25bf710e7d0a295f
hasnainzia20 <hasnainzia438@gmail.com>
2023-10-09 22:42:13 -0700

    finished project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCard from "./components/HeadlineCard";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCard />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
